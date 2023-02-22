3eb3455068fec44e4240b7dfdc24f665c53e59e1
layla <cisneroslay@hotmail.com>
2023-02-22 10:00:00 -0300

    New proyecto

---
+++ LocationTest/LocationTest/LocationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationTestApp.swift
+//  LocationTest
+//
+//  Created by Layla Cisneros on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LocationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
